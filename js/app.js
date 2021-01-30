62868ead33224cb85aacd28eeab5d1ca782a0452
jscblack <jscblack@163.com>
2021-01-30 19:49:45 +0800

    test

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*
+ * @Author       : Gehrychiang
+ * @LastEditTime : 2021-01-30 19:48:26
+ * @Website      : www.yilantingfeng.site
+ * @E-mail       : gehrychiang@aliyun.com
+ * @ProbTitle    : (记得补充题目标题)
+ */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
